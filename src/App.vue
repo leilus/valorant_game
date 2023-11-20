0edbb76e75d17e68dedec6a55512a309ea0879d5
leilus <prettytigerr@gmail.com>
2023-11-20 18:13:36 +0100

    Changes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-  <div>
-    <h1>Valorant</h1>
+  <div class="widget">
+  <div class="container">
+    <h1>Guess the Valorant skin!</h1>
     <div v-if="randomSkin">
-      <h2>{{ randomSkin.displayName  }}</h2>
       <img :src="randomSkin.displayIcon" />
     </div>
     <div v-else>
       <p>Brak danych do wyświetlenia.</p>
     </div>
-    <input v-model="enteredName" placeholder="Wpisz aktualną nazwę obiektu" />
+    <div v-if="showName && nameDisplayed">  {{ randomSkin.displayName  }}</div>
+
+    <input v-model="enteredName" placeholder="Write name of the skin!" />
     <button @click="checkSkin">Check!</button>
+    <button @click="reset"> I don't know </button>
+    <button @click="toggleShowName">Show Hint</button>
   </div>
-  <HighScore />
+  <div class="sidebar">
+  <HighScore :resultOf="result" />
+  </div>
+</div>
+
 </template>
 
 <script>
 import axios from 'axios'
 import HighScore from './components/HighScore.vue';
+
 
 export default {
   components: {
@@ -29,6 +38,9 @@ export default {
       skins: [], 
       randomSkin: null,  
       enteredName: '',
+      result: null,
+      showName: false,
+      nameDisplayed: false,
     };
   },
   async mounted() {
@@ -48,40 +60,96 @@ export default {
       const randomIndex = Math.floor(Math.random() * this.skins.length);
       this.randomSkin = this.skins[randomIndex];
     },
+    reset(){
+      const randomIndex = Math.floor(Math.random() * this.skins.length);
+      this.randomSkin = this.skins[randomIndex];
+      this.result = 0;
+      this.nameDisplayed = false;
+    },
+    toggleShowName(){
+      this.showName = true;
+      this.nameDisplayed = true;
+    },
     checkSkin() {
-  const enteredNameLower = this.enteredName.toLowerCase();
-  const skinNameLower = this.randomSkin.displayName.toLowerCase();
-  let result = 0;
+    const enteredNameLower = this.enteredName.toLowerCase();
+    const skinNameLower = this.randomSkin.displayName.toLowerCase();
 
-  if (skinNameLower.includes("vandal")) {
-    if (enteredNameLower === skinNameLower.replace("vandal", "").trim()) {
-      result = result + 1;
-      this.selectRandomSkin();
-    } else {
-      console.log("pa");
+      if (skinNameLower.includes("vandal")) {
+        if (enteredNameLower === skinNameLower.replace("vandal", "").trim()) {
+          if(this.showName === !this.showName){
+          this.result += 0.5;
+          this.nameDisplayed = false;
+          }else{
+            this.result += 1;
+            this.nameDisplayed = false;
+          }
+        this.selectRandomSkin();
+      } else {
+        this.showName = true;
+      }
+  } 
+      else if (skinNameLower.includes("phantom")) {
+        if (enteredNameLower === skinNameLower.replace("phantom", "").trim()) {
+          if(this.showName === !this.showName){
+          this.result += 0.5;
+          }else{
+            this.result += 1;
+          }
+        this.selectRandomSkin();
+      } else {
+        this.showName = true;
     }
-  } else if (skinNameLower.includes("phantom")) {
-    if (enteredNameLower === skinNameLower.replace("phantom", "").trim()) {
-      result = result + 1;
-      this.selectRandomSkin();
-    } else {
-      console.log("pa po");
-    }
-  } else {
-    console.log("dupa");
+  }   else {
+      console.log("dupa");
   }
-  console.log(result)
-  this.enteredName = '';
+
+    this.enteredName = '';
 },
 
   }
   
 };
 
-
-
 </script>
 
-
 <style scoped>
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body, html{
+  height: 100%;
+  margin: 0;
+}
+
+.widget{
+  height: 100vh;
+  background-image: url(./img/valorant_background.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+}
+
+.container{
+  background-color: white;
+  padding: 20px;
+  height: 300px;
+  width: 700px;
+  text-align: center;
+}
+
+.container h1{
+  text-align: center;
+}
+
+.sidebar{
+  background-color: beige;
+  height: 300px;
+  width: 300px;
+}
 </style>
--- src/components/HighScore.vue
+++ src/components/HighScore.vue
@@ -1,6 +1,15 @@
 <template>
     <h1>Your highscore!</h1>
-    
+    <p>{{ resultOf }}</p>
 </template>
 <script>
+export default{
+    props: ['resultOf'],
+};
 </script>
+
+<style scoped>
+p{
+    text-align: center;
+}
+</style>
